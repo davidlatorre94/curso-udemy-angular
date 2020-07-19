05b9fddbf843fd416adc20f9dfc06efbaf07561e
David Latorre <davidlatorre94@hotmail.com>
2020-07-18 21:05:20 -0500

    first commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+ {id: 1, nombre: 'David', apellido: 'Latorre', email: 'abc@ua.com', createAt: '2020-07-06'},
+ {id: 2, nombre: 'Paola', apellido: 'Jara', email: 'pj@ua.com', createAt: '2021-07-06'},
+ {id: 3, nombre: 'Martin Elias', apellido: 'Diaz', email: 'meda@ua.com', createAt: '2022-07-06'},
+ {id: 4, nombre: 'Nombre 4', apellido: 'Apellido 4', email: 'n4a4@ua.com', createAt: '2023-07-06'},
+ {id: 5, nombre: 'Nombre5', apellido: 'Apellido 5', email: 'n5a5@ua.com', createAt: '2024-07-06'}
+]
